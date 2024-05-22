9f7368b86001bd000842cbabc6736acaa0e36263
SouthernMix <sebastian-0518@hotmail.com>
2024-05-21 20:08:23 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Portafolio</title>
+</head>
+<body>
+    <header>
+        <div class="navbar_logo">
+            <div class="logo1">
+                <a href="#home"><img src="./images/100px-logo.png"></a>
+            </div>
+            <div class="logo2">
+                <a href="#home"><img src="./images/100px-logo-purple.png"></a>
+            </div>
+        </div>
+        <ul class="navbar">
+            <li>
+                <a href="#home">Home</a>
+            </li>
+            <li>
+                  <a href="#about">About me</a>
+            </li>
+            <li>
+                <a href="#studies">Studies</a>
+            </li>
+            <li>
+                <a href="#projects">Projects</a>
+            </li>
+            <li>
+                <a href="#contact">Contact Me</a>
+            </li>
+        </ul>
+        <div class='bx bx-menu' id="menu-icon"></div>
+    </header>
+    <section class="home" id="home">
+        <div class="home-content">
+            <div class="home_img">
+                <img src="./images/pfp.jpg" alt="foto de perfil">
+            </div>
+            <div class="home_text">
+                <h1>Joao Sebastián Díaz Pava</h1>
+                <p>I'm Joao Sebastián, also known as SouthernMix on internet, I really like things related to Astronomy, Technology and Science. Actually, I’m in fourth semester of System Engineering. In my free time I like to listen to music or play videogames.</p>
+            </div>
+        </div>
+        <div class="home-content-down">
+            <div class="item">
+                <h4>Email</h4>
+                <a href="mailto:sebastian-0518@hotmail.com">sebastian-0518@hotmail.com</a>
+            </div>
+            <div class="item">
+                <h4>Phone Number</h4>
+                <p>(+57) 319 271 0972</p>
+            </div>
+            <div class="item">
+                <h4>Linkedin</h4>
+                <a href="https://www.linkedin.com/in/joao-s-díaz-9b4b0a306/">@Joao S. Díaz</a>
+            </div>
+            <div class="item">
+                <h4>GitHub</h4>
+                <a href="https://github.com/SouthernMix">@SouthernMix</a>
+            </div>
+        </div>
+    </section>
+    <section class="about" id="about">
+        <h1> - About Me - </h1>
+        <div class="content_box">
+            <div class="content_img">
+                <img src="./images/pfp.jpg" alt="imagen de perfil">
+            </div>
+            <div class="content_text">
+                <p>Hi, my name is Joao Sebastián, also known as SouthernMix on internet, I really like things related to Astronomy, Technology and Science. Actually, I’m in fourth semester of System Engineering. In my free time I like to listen to music or play videogames.</p>
+            </div>
+        </div>
+        <div class="content_box">
+            <div class="content_text">
+                <p>I'm a huge fan of everything related to space. I'm not much into watching movies, but I must admit that the movie "Interstellar" was great. I strongly believe that technology and astronomy will be the future of the human race. Documentaries about space also catch my attention a lot.</p>
+            </div>
+            <div class="content_img">
+                <img src="./images/trucos-para-orientarse-en-el-espacio-interestelar.jpg" alt="space">
+            </div>
+        </div>
+        <div class="content_box">
+            <div class="content_img">
+                <img src="./images/mobile.jpg" alt="edm">
+            </div>
+            <div class="content_text">
+                <p>As I mentioned before, I really enjoy listening to music in my free time or basically while doing anything else. I move a lot between different genres, with my favorite type of music being EDM. I can go to Rock-EDM oriented, come back to Spanish Bachata or 2012 Reggaeton, and end up listening to mainstream Pop-Rock. I consider myself very flexible when it comes to music. Music is life.</p>
+            </div>
+        </div>
+    </section>
+    <section class="studies" id="studies">
+        <h1> - Studies - </h1>
+        <div class="content-image">
+            <div class="imagen">
+                <img src="./images/232-2329789_corporacin-universitaria-minuto-de-dios-logo-uniminuto-png.png" alt="">
+                <p>2022 to date</p>
+                <div class="overlay">
+                    <h2><i class='bx bx-time-five'></i> 4th Semester.<br><i class='bx bx-book-open' ></i> System Engineering.<br><i class='bx bxs-school' ></i> Uniminuto University.</h2>
+                </div>
+            </div>
+            <div class="imagen">
+                    <img src="./images/sena logo.png" alt="">
+                    <p>June 2018 to December 2018</p>
+                    <div class="overlay">
+                        <h2><i class='bx bx-time-five'></i> 6 months.<br><i class='bx bx-book-open' ></i> System Technician.<br><i class='bx bxs-school' ></i> SENA.</h2>
+                    </div>
+            </div>
+            <div class="imagen">
+                    <img src="./images/sena logo.png" alt="">
+                    <p>June 2018</p>
+                    <div class="overlay">
+                        <h2><i class='bx bx-time-five'></i> 80 hours.<br><i class='bx bx-book-open' ></i> Excel for business.<br><i class='bx bxs-school' ></i> SENA.</h2>
+                    </div>
+            </div>
+            <div class="imagen">
+                    <img src="./images/AProgramar.jpg" alt="">
+                    <p>2023</p>
+                    <div class="overlay">
+                        <h2><i class='bx bx-time-five'></i> 5 weeks.<br><i class='bx bx-book-open' ></i> Programming Intro.<br><i class='bx bxs-school' ></i> Edinburgh University.</h2>
+                    </div>
+            </div>
+            <div class="imagen">
+                    <img src="./images/intelartif.jpg" alt="">
+                    <p>2023</p>
+                    <div class="overlay">
+                        <h2><i class='bx bx-time-five'></i> 4 weeks.<br><i class='bx bx-book-open' ></i> Human Centered AI.<br><i class='bx bxs-school' ></i> Monterrey Tecnologic.</h2>
+                    </div>
+            </div>
+            <div class="imagen">
+                    <img src="./images/concep exc analisis datos.jpg" alt="">
+                    <p>2022</p>
+                    <div class="overlay">
+                        <h2><i class='bx bx-time-five'></i> 5 weeks.<br><i class='bx bx-book-open' ></i> Excel Basic Concepts.<br><i class='bx bxs-school' ></i> IBM.</h2>
+                    </div>
+            </div>
+            <div class="imagen">
+                    <img src="./images/BusinessTransformation.jpg" alt="">
+                    <p>2022</p>
+                    <div class="overlay">
+                        <h2><i class='bx bx-time-five'></i> 4 weeks.<br><i class='bx bx-book-open' ></i> Business Transformation.<br><i class='bx bxs-school' ></i> Google.</h2>
+                    </div>
+            </div>
+            <div class="imagen">
+                    <img src="./images/trans figital.jpg" alt="">
+                    <p>2022</p>
+                    <div class="overlay">
+                        <h2><i class='bx bx-time-five'></i> 5 weeks.<br><i class='bx bx-book-open' ></i> Digital Transformation.<br><i class='bx bxs-school' ></i> Virginia University.</h2>
+                    </div>
+            </div>
+        </div>
+
+    </section>
+    <section class="projects" id="projects">
+        <h1> - Projects - </h1>
+        <div class="content">
+            <div class="content_box">
+                <div class="content_img">
+                    <img src="./images/brewed.jpg" alt="imagen de perfil">
+                </div>
+                <div class="content_text">
+                    <h4>Brewed Bliss</h4>
+                    <p>A Coffee Shop Oriented APP for Coffee Shopping.</p>
+                </div>
+            </div>
+            <div class="content_box">
+                <div class="content_img">
+                    <img src="./images/Rectangle 1.png" alt="space">
+                </div>
+                <div class="content_text">
+                    <h4>Home Juan</h4>
+                    <p>A website oriented to buy, sell, rent houses, apartments, weekend country houses and plots.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="contact" id="contact">
+        <h1> - Contact Me - </h1>
+        <div class="contact_box">
+            <div class="item">
+                <p>Your Name:</p>
+                <input class="text" type="text" placeholder="Write your name">
+            </div>
+            <div class="item">
+                <p>Your Email:</p>
+                <input class="text" type="text" placeholder="example@example.com">
+            </div>
+            <div class="item">
+                <p>Your Message:</p>
+                <input class="text msg" type="text" placeholder="Write your message">
+            </div>
+            <div class="item">
+                <button>Send Message</button>
+            </div>
+        </div>
+    </section>
+    <div class="follow">
+        <h2>Follow Me:</h2>
+        <div class="social-media">
+            <i class='bx bxl-github' ></i>
+            <i class='bx bxl-google'></i>
+            <i class='bx bxl-facebook-circle'></i>
+            <i class='bx bxl-twitter' ></i>
+            <i class='bx bxl-instagram' ></i>
+            <i class='bx bxl-discord-alt' ></i>
+            <i class='bx bxl-linkedin'></i>
+        </div>
+</div>
+    <footer>
+    <div class="copyright">
+        <p>SouthernMix Company LLC.</p>
+        <p>Design fully made by SouthernMix using CSS, HTML & JavaScript.</p>
+        <p><i class='bx bx-copyright'></i> Copyright 2024. All rights reserved.</p>
+    </div>
+    </footer>
+<script src="./scripts/script.js"></script>
+</body>
+</html>
